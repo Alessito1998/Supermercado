b65fcab6df5c1b8ee2972c56db773458dce64de4
Alex <alexgaggero159@gmail.com>
2023-01-17 12:31:42 +0100

    modificado el syso

--- Supermercado/src/supermercado1/SupermercadoMain.java
+++ Supermercado/src/supermercado1/SupermercadoMain.java
@@ -3,7 +3,7 @@ package supermercado1;
 public class SupermercadoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Bienvenido al supermercadooooo");
 
 	}
 
